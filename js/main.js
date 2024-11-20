0dfe3097b7f53c5d0869a2d2e0a24cb47fb8e58f
Otabekcdr <boltaboyevotabek207@gmail.com>
2024-11-20 17:12:56 +0500

    completed full page with resposive

--- js/main.js
+++ js/main.js
@@ -1,32 +1,42 @@
-// Hamburger animation -------------------------------------------------------------------------
-document.getElementById("menu-icon").addEventListener("click", function () {
-    this.classList.toggle("active")
-})
+let loader = document.querySelector(".loader"),
+    body = document.querySelector("body")
+window.addEventListener("load", () => {
+    loader.classList.add("loader_active")
+    let interval = setInterval(removeLoader, 300)
 
-// Swiper -------------------------------------------------------------------------
-var swiper = new Swiper(".mySwiper", {
-    pagination: {
-        el: ".swiper-pagination",
-        type: "progressbar",
-    },
-    navigation: {
-        nextEl: ".swiper-next",
-        prevEl: ".swiper-prev",
-    },
-    loop: true,
+    function removeLoader() {
+        body.removeChild(loader)
+        clearInterval(interval)
+    }
 
-    breakpoints: {
-        100: {
-            slidesPerView: 1,
-            spaceBetween: 20,
+    // Hamburger animation -------------------------------------------------------------------------
+    document.getElementById("menu-icon").addEventListener("click", function () {
+        this.classList.toggle("active")
+    })
+
+    // Swiper -------------------------------------------------------------------------
+    var swiper = new Swiper(".mySwiper", {
+        pagination: {
+            el: ".swiper-pagination",
+            type: "progressbar",
         },
-        768: {
-            slidesPerView: 1,
-            spaceBetween: 20,
+        navigation: {
+            nextEl: ".swiper-next",
+            prevEl: ".swiper-prev",
         },
-        1024: {
-            slidesPerView: 2,
-            spaceBetween: 20,
+        loop: true,
+
+        breakpoints: {
+            100: {
+                slidesPerView: 1,
+            },
+            768: {
+                slidesPerView: 1,
+            },
+            1024: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
         },
-    },
+    })
 })
